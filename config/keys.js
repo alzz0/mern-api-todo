98051965bdbbbee309e483d38e346ad08f306eae
Ali <alimansour97@gmail.com>
2018-08-27 01:04:02 -0600

    add item modal

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports ={
+    mongoURI:"mongodb://alzz:operations99@ds229722.mlab.com:29722/mern"
+}
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const bodyParser= require("body-parser")
+const items= require("./routes/api/items")
+
+const app = express();
+
+
+
+app.use(bodyParser.json())
+
+
+
+
+//db config
+const db=require("./config/keys.js").mongoURI;
+
+//connect mongodb
+
+mongoose.connect(db)
+.then(()=>console.log("mongodb Connected"))
+.catch(err=> console.log(err))
+
+// use routes
+
+app.use("/api/items",items)
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port,()=>console.log(`Server Started on port: ${port}`))
